9532d23ab14a4e851995d6bc2c8d308ad8bc78e2
Steve Shipsey <sashipsey@gmail.com>
2023-12-01 12:56:05 -0500

    cleanup day 1

--- day1/q.ts
+++ day1/q.ts
@@ -1,10 +1,22 @@
 import fs from 'fs';
 
 fs.readFile('input', 'utf-8', (err, inp) => {
-  let answer = 0;
-  // multi line
-  const arr = inp.split('\r\n');
-  for (let line of arr) {
+  const part1 = () => {
+    let answer = 0;
+    // multi line
+    const arr = inp.split('\r\n');
+
+    for (let line of arr) {
+      line = line.replace(/[a-z]/g, '');
+      answer += +(line[0] + line[line.length - 1]);
+    }
+    return answer;
+  };
+
+  const part2 = () => {
+    let answer = 0;
+    // multi line
+    const arr = inp.split('\r\n');
     const vals: { [key: string]: any } = {
       zero: 0,
       one: 1,
@@ -18,27 +30,30 @@ fs.readFile('input', 'utf-8', (err, inp) => {
       nine: 9,
     };
 
-    let currentWord = '';
-    let v = '';
-    for (let i = 0; i < line.length; ++i) {
-      currentWord += line[i];
-      if (line[i].match(/\d/)) {
-        v += line[i];
-      }
-      for (let val of Object.keys(vals)) {
-        if (currentWord.includes(val)) {
-          v += vals[val];
-          currentWord = '';
+    for (let line of arr) {
+      let wordBuilder = '';
+      let lineNumbersString = '';
+      for (let char of line) {
+        if (!isNaN(+char)) {
+          lineNumbersString += char;
+          wordBuilder = '';
+          continue;
+        }
+        wordBuilder += char;
+        for (let val of Object.keys(vals)) {
+          if (wordBuilder.includes(val)) {
+            lineNumbersString += vals[val];
+            wordBuilder = '';
+          }
         }
       }
+
+      answer += +(
+        lineNumbersString[0] + lineNumbersString[lineNumbersString.length - 1]
+      );
     }
-    // let x = line.replace(/\d/g, '');
-    // for (let val of Object.keys(vals)) {
-    //   x = x.replace(val, vals[val]);
-    // }
-    // x = x.replace(/[a-z]/g, '');
-    const val = +(v[0] + v[v.length - 1]);
-    answer += val;
-  }
-  console.log(answer);
+    return answer;
+  };
+
+  console.log(part1(), part2());
 });
