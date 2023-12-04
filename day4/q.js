412da01752a0fadf138f941f5e3a4daea8a67483
Steve Shipsey <sashipsey@gmail.com>
2023-12-04 01:05:40 -0500

    day4

---
+++ day4/q.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+
+fs.readFile('input', 'utf-8', (err, inp) => {
+  const processCard = (line) => {
+    const l = line.split(': ')[1].split(' | ');
+    return [
+      l[0].replaceAll(/\s+/g, ' ').trim().split(' '),
+      l[1].replaceAll(/\s+/g, ' ').trim().split(' '),
+    ];
+  };
+
+  const cards = inp.split('\r\n');
+
+  const part1 = () =>
+    cards.reduce((score, card) => {
+      const [winning, myNums] = processCard(card);
+      return (
+        score +
+        myNums.reduce(
+          (acc, num) => (winning.includes(num) ? Math.max(acc * 2, 1) : acc),
+          0
+        )
+      );
+    }, 0);
+
+  const part2 = () => {
+    let cardsCounts = Array(cards.length).fill(1);
+    for (let i = 0; i < cards.length; i++) {
+      const [winning, myNums] = processCard(cards[i]);
+
+      const matchCount = myNums.reduce(
+        (matches, num) => (winning.includes(num) ? ++matches : matches),
+        0
+      );
+
+      for (let j = 0; j < matchCount; ++j) {
+        cardsCounts[i + 1 + j] += cardsCounts[i];
+      }
+    }
+    return cardsCounts.reduce((a, b) => a + b);
+  };
+
+  console.log(part1(), part2());
+});
